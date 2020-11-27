89930a8f49526e4591b7f0eb81e6a853dd4d2fe5
Achiraisuru <achiraisuru@gmail.com>
2020-11-27 20:53:46 +0530

    final styles

--- src/Admin/Listening/Viewpodcast.js
+++ src/Admin/Listening/Viewpodcast.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
@@ -32,7 +31,10 @@ const ViewPods = () => {
 //   const [readings, setReadings] = useState([]);
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [oldtext,setoldtext] = useState("Hello world");
+  // const [newtext,setnewtext] = useState("");
+  // const [per,setper]=useState();
 
 //   const getreadings = async () => {
 //     const results = await Axios({
@@ -54,6 +56,54 @@ const ViewPods = () => {
 //         (read) =>
 //           read.ndate.includes(searchTerm) || read.title.includes(searchTerm)
 //       );
+
+// const check = (s1,s2) =>{
+//   console.log("clicked")
+//   let longer = s1;
+//   let shorter = s2;
+//   if (s1.length < s2.length) {
+//     longer = s2;
+//     shorter = s1;
+//   }
+//   let longerLength = longer.length;
+//   if (longerLength === 0) {
+//     return 1.0;
+//   }
+//   setper((longerLength - editDistance(longer, shorter)) / parseFloat(longerLength));
+//   return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+
+  
+// }
+
+// function editDistance(s1, s2) {
+//   s1 = s1.toLowerCase();
+//   s2 = s2.toLowerCase();
+
+//   var costs = new Array();
+  
+//   for (var i = 0; i <= s1.length; i++) {
+//     var lastValue = i;
+//     for (var j = 0; j <= s2.length; j++) {
+//       if (i == 0)
+//         costs[j] = j;
+//       else {
+//         if (j > 0) {
+//           var newValue = costs[j - 1];
+//           if (s1.charAt(i - 1) != s2.charAt(j - 1))
+//             newValue = Math.min(Math.min(newValue, lastValue),
+//               costs[j]) + 1;
+//           costs[j - 1] = lastValue;
+//           lastValue = newValue;
+//         }
+//       }
+//     }
+//     if (i > 0)
+//       costs[s2.length] = lastValue;
+//   }
+//   return costs[s2.length];
+//   console.log(costs[s2.length])
+// }
+
 
 
 
@@ -145,6 +195,14 @@ expect in future episodes.
           </Accordion>
         </div>
       {/* ))} */}
+
+
+      {/* <input className="" type="text" onChange={(e) => setnewtext(e.target.value)}/>
+     
+      <div>{oldtext}</div>
+      <div>{newtext}</div>
+      <button onClick={() =>check(oldtext,newtext)}>test</button>
+      <div>{per*100}</div> */}
     </div>
   );
 };
